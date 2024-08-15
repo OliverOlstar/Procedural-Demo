6a6145821443dad0b2b05647725d1719ffc4b518
OliverOlstar <48457872+OliverOlstar@users.noreply.github.com>
2024-08-15 14:43:38 -0700

    Changed leaning to spring damper system

--- ProceduralDemo/Assets/Game/Characters/Model/CharacterModelLeaning.cs
+++ ProceduralDemo/Assets/Game/Characters/Model/CharacterModelLeaning.cs
@@ -13,24 +13,31 @@ public class CharacterModelLeaning : CharacterModelControllerBase
 	[SerializeField]
 	private float m_MaxAcceleration = 15.0f;
 	[SerializeField]
-	private float m_Dampening = 2.0f;
+	private float m_Spring = 100.0f;
+	[SerializeField]
+	private float m_Damper = 10.0f;
+	[SerializeField]
+	private float m_Scalar = 2.0f;
 
-	private Quaternion m_LastRotation = Quaternion.identity;
-	public Quaternion CalculatedRotation => m_LastRotation;
+	private Quaternion m_CurrRotation = Quaternion.identity;
+	public Quaternion CalculatedRotation => m_CurrRotation;
+
+	private Vector3 m_Direction = Vector3.zero;
+	private Vector2 m_Velocity = Vector2.zero;
 
 	public override void Reset()
 	{
-		m_LastRotation = Quaternion.identity;
+		m_CurrRotation = Quaternion.identity;
 	}
 
 	public override void Tick(float pDeltaTime)
 	{
 		GetMovement(out Vector3 direction, out float amount01);
-		Quaternion rotationToTarget = Quaternion.FromToRotation(Vector3.up, direction);
-		rotationToTarget = Quaternion.Lerp(Quaternion.identity, rotationToTarget, amount01);
-		m_LastRotation = Quaternion.Lerp(m_LastRotation, rotationToTarget, pDeltaTime * m_Dampening);
-		// Quaternion targetRotation = Quaternion.identity * rotationToTarget;
-		// m_Transform.localRotation = Quaternion.Lerp(m_Transform.localRotation, targetRotation, pDeltaTime * m_Dampening);
+		m_Velocity += amount01 * m_Scalar * new Vector2(direction.x, direction.z);
+		m_Direction = new Vector3(
+			Func.SpringDamper(m_Direction.x, 0, ref m_Velocity.x, m_Spring, m_Damper, pDeltaTime), 1.0f,
+			Func.SpringDamper(m_Direction.z, 0, ref m_Velocity.y, m_Spring, m_Damper, pDeltaTime));
+		m_CurrRotation = Quaternion.FromToRotation(Vector3.up, m_Direction.normalized);
 	}
 
 	private Vector3 m_LastVelocity = Vector3.zero;
