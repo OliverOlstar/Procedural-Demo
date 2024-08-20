681e0f5ac7e54cdc2923aa65cbfcf6e22ae1b46a
OliverOlstar <48457872+OliverOlstar@users.noreply.github.com>
2024-08-19 21:28:30 -0700

    Fixed Walk Animation offset Fixed CheatManager warnings

--- ProceduralDemo/Assets/Game/CheatManager.cs
+++ ProceduralDemo/Assets/Game/CheatManager.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using ODev;
 using UnityEngine;
 
-public class CheatManager : MonoBehaviour
+public class CheatManager : UpdateableMonoBehaviour
 {
 	[SerializeField, Range(0.0f, 2.0f)]
 	private float m_TimeScale = 1.0f;
 
+	private float m_LastTimeScale = 1.0f;
 	private int m_TimeScaleKey = -1;
 
-    private void OnValidate()
+	protected override void Tick(float pDeltaTime)
 	{
-		if (!Application.isPlaying)
+		if (m_TimeScale == m_LastTimeScale)
 		{
 			return;
 		}
+		m_LastTimeScale = m_TimeScale;
+		
 		if (m_TimeScaleKey != -1)
 		{
 			TimeScaleManager.UpdateTimeEvent(m_TimeScaleKey, m_TimeScale, true);
