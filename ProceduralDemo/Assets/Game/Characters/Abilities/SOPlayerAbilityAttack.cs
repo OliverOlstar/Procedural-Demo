e63d5a00c6698fdf1fca658741648b8410f3804c
OliverOlstar <48457872+OliverOlstar@users.noreply.github.com>
2024-08-26 22:52:11 -0700

    Started attack ability

---
+++ ProceduralDemo/Assets/Game/Characters/Abilities/SOPlayerAbilityAttack.cs
@@ -0,0 +1,72 @@
+using ODev.Input;
+using ODev.Picker;
+using ODev.PoseAnimator;
+using ODev.Util;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "New Attack Ability", menuName = "Character/Ability/Player Attack")]
+public class SOPlayerAbilityAttack : SOCharacterAbility
+{
+	[Space, SerializeField, AssetNonNull]
+	private SOPoseMontage m_Montage = null;
+
+	[Space, SerializeField]
+	private AnimationCurve m_VelocityCurve = new();
+	[SerializeField]
+	private float m_Velocity = 0.0f;
+
+	public SOPoseMontage Montage => m_Montage;
+	public AnimationCurve VelocityCurve => m_VelocityCurve;
+	public float Velocity => m_Velocity;
+
+	public override ICharacterAbility CreateInstance(PlayerRoot pPlayer, UnityAction pOnInputPerformed, UnityAction pOnInputCanceled) => new PlayerAbilityAttack(pPlayer, this, pOnInputPerformed, pOnInputCanceled);
+}
+
+public class PlayerAbilityAttack : CharacterAbility<SOPlayerAbilityAttack>
+{
+	public PlayerAbilityAttack(PlayerRoot pPlayer, SOPlayerAbilityAttack pData, UnityAction pOnInputPerformed, UnityAction pOnInputCanceled) : base(pPlayer, pData, pOnInputPerformed, pOnInputCanceled) { }
+
+	public override IInputTrigger InputActivate => Root.Input.AttackPrimary;
+
+	protected override void Initalize() { }
+	protected override void DestroyInternal() { }
+
+	private Vector3 m_Direction;
+	private int m_MontageHandle = PoseMontageAnimator.NULL_HANDLE;
+	private float m_TimeElapsed;
+
+	protected override bool CanActivate()
+	{
+		return Root.OnGround.IsOnGround;
+	}
+
+	protected override void ActivateInternal()
+	{
+		Root.Movement.MovementEnabled = false;
+		m_Direction = Root.Animator.transform.forward.Horizontalize();
+		m_MontageHandle = Root.Animator.PlayMontage(Data.Montage);
+		m_TimeElapsed = 0.0f;
+	}
+
+	public override void ActiveTick(float pDeltaTime)
+	{
+		m_TimeElapsed += pDeltaTime;
+		float seconds = Data.Montage.TotalSeconds - Data.Montage.FadeOutSeconds;
+		if (m_TimeElapsed >= seconds)
+		{
+			m_MontageHandle = PoseMontageAnimator.NULL_HANDLE;
+			Deactivate();
+			return;
+		}
+		float progress01 = m_TimeElapsed / seconds;
+		float velocity = Data.VelocityCurve.Evaluate(progress01) * Data.Velocity;
+		Root.Movement.AddVelocityXZ(m_Direction * velocity);
+	}
+
+	protected override void DeactivateInternal()
+	{
+		Root.Movement.MovementEnabled = true;
+		Root.Animator.CancelMontage(m_MontageHandle);
+	}
+}
